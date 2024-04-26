2c68f4dc3092038dede6dff2f444d4b62448c253
edison.estramil <edison.estramil@fing.edu.uy>
2024-04-26 19:30:35 -0300

    a

--- All.v1i.yolov9/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# All > 2024-04-24 4:31pm
-https://universe.roboflow.com/steven-qah0z/all-wzinz
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- prueba.py
+++ prueba.py
@@ -1,4 +1,4 @@
-from  ultralytics import YOLO
+from ultralytics import YOLO
 
 # Build a YOLOv9c model from scratch
 model = YOLO('yolov9c.pt')
@@ -6,6 +6,8 @@ model = YOLO('yolov9c.pt')
 # Display model information (optional)
 model.info()
 
-results = model.train(data='All.v2.yolov9/data.yaml', epochs=200, imgsz=640)
+# Train using GPU with reduced batch size and reduced number of workers
+results = model.train(data='All.v2.yolov9/data.yaml', epochs=200, imgsz=640, batch=8, workers=4, device='cuda')
 
+# Save weights
 model.save_weights('/clusteruy/home/edison.estramil/weights.h5')
