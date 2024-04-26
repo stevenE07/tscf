c435f81d1fd0504d02968736f721b508a63ca43c
edison.estramil <edison.estramil@fing.edu.uy>
2024-04-26 16:09:18 -0300

    a

--- script.sh
+++ script.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=neuronalnetwork
+#SBATCH --job-name=mitrabajo
 #SBATCH --ntasks=1
-#SBATCH --mem=2048
-#SBATCH --time=2:00:00
-#SBATCH --tmp=9G
-#SBATCH --partition=normal
-#SBATCH --qos=normal
+#SBATCH --mem=512
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edison.estramil@fing.edu.uy
+
+# de acuerdo a lo que quiera ejecutar puede elegir entre las siguientes tres líneas.
+#SBATCH --gres=gpu:1 # se solicita una gpu cualquiera( va a tomar la primera que quede disponible indistintamente si es una p100 o una a100)
+#SBATCH --gres=gpu:p100:1 # se solicita una nvidia p100
+#SBATCH --gres=gpu:a100:1 # se solicita una nvidia a100, tenga en cuenta que solamente hay 2 a100 disponibles en toda la ifraestructura y están ubicadas en servidores diferentes (node15 y node16)
+
+
+
+#SBATCH --partition=normal
+#SBATCH --qos=gpu
+
 
 source /etc/profile.d/modules.sh
 
