4ddb3e80a58502c5585d536ca7b1a7ccbba97255
edison.estramil <edison.estramil@fing.edu.uy>
2024-04-27 14:11:11 -0300

    ultimo

--- ambiente.sh
+++ ambiente.sh
@@ -1,3 +1,5 @@
+srun --job-name=mitrabajo --time=4:00:00 --partition=besteffort --qos=besteffort --ntasks=1 --mem=2048 --gres=gpu:1 --pty bash -l
+
 cd /scratch/edison.estramil
 
 git clone https://github.com/stevenE07/tscf.git
@@ -6,8 +8,10 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /scratch/edison.estramil/miniconda3
 
-source mini/bin/activate
+source miniconda3/bin/activate
 
 pip install ultralytics
+
+cd tscf
 
 python prueba.py
--- prueba.py
+++ prueba.py
@@ -7,7 +7,4 @@ model = YOLO('yolov9c.pt')
 model.info()
 
 # Train using GPU with reduced batch size and reduced number of workers
-results = model.train(data='All.v2.yolov9/data.yaml', epochs=2, imgsz=640, batch=8, workers=1, device='cuda')
-
-# Save weights
-model.save_weights('/clusteruy/home/edison.estramil/weights.h5')
+results = model.train(data='All.v2.yolov9/data.yaml', epochs=100, imgsz=640, workers=0, device='cuda')
