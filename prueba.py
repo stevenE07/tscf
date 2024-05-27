ac3d9d7ee879101848ab75926acd99f26b6fac00
edison.estramil <edison.estramil@fing.edu.uy>
2024-05-27 12:17:24 -0300

    a

--- prueba.py
+++ prueba.py
@@ -7,4 +7,4 @@ model = YOLO('yolov9c.pt')
 model.info()
 
 # Train using GPU with reduced batch size and reduced number of workers
-results = model.train(data='All.v2.yolov9/data.yaml', epochs=100, imgsz=640, workers=0, device='cuda')
+results = model.train(data='solo_lolium_20/data.yaml',batch=16, epochs=100, imgsz=640, workers=0, device='cuda')
